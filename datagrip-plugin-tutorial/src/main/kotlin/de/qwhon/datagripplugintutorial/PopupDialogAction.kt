66a63ec394ddb8525439d5d9a514e5e18fbd451a
Frank Schmitt <frankschmitt@gmx.de>
2023-09-23 00:07:56 +0200

    show file chooser dialog in action

--- datagrip-plugin-tutorial/src/main/kotlin/de/qwhon/datagripplugintutorial/PopupDialogAction.kt
+++ datagrip-plugin-tutorial/src/main/kotlin/de/qwhon/datagripplugintutorial/PopupDialogAction.kt
@@ -2,6 +2,8 @@ package de.qwhon.datagripplugintutorial
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.fileChooser.FileChooser
+import com.intellij.openapi.fileChooser.FileChooserDescriptor
 
 class PopupDialogAction : AnAction() {
     override fun update(event: AnActionEvent) {
@@ -10,6 +12,8 @@ class PopupDialogAction : AnAction() {
     }
 
     override fun actionPerformed(event: AnActionEvent) {
+        val descriptor = FileChooserDescriptor(true, false, false, false, false, false)
+        FileChooser.chooseFiles(descriptor, null, null)
         // Using the event, implement an action.
         // For example, create and show a dialog.
     } // Override getActionUpdateThread() when you target 2022.3 or later!
